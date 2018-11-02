df82df925d8eed459030bc13f6cc3df9c516a994
a_m_a_n <amanbhayana@yahoo.in>
2018-11-02 14:42:46 -0400

    Linear regression, Logistic regression and neural network completed successfully.

--- Regression_Neural.py
+++ Regression_Neural.py
@@ -4,10 +4,10 @@ import csv
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib
 import random as random
 
-
-def GenerateRawData(filePath, IsSynthetic = False):
+def GenerateRawData(filePath):
     dataMatrix = []
     with open(filePath, 'rU') as fi:
         reader = csv.reader(fi)
@@ -159,7 +159,7 @@ def linearReggression( inputFilename):
 
 
     RawTarget = GetTargetVector(inputFilename )
-    RawData   = GenerateRawData(inputFilename,IsSynthetic)
+    RawData   = GenerateRawData(inputFilename)
 
     TrainingTarget = np.array(GenerateTrainingTarget(RawTarget,TrainingPercent))
     TrainingData   = GenerateTrainingDataMatrix(RawData,TrainingPercent)
@@ -191,14 +191,14 @@ def linearReggression( inputFilename):
 
     W_Now = np.dot(220, W)
     La = 2
-    learningRate = 0.04
+    learningRate = 0.05
     L_Erms_Val = []
     L_Erms_TR = []
     L_Erms_Test = []
     W_Mat = []
 
     # This loop is to iterate over the phi matrix until convergence
-    for i in range(0, 500):
+    for i in range(0, 600):
         # print ('---------Iteration: ' + str(i) + '--------------')
         Delta_E_D = -np.dot((TrainingTarget[i] - np.dot(np.transpose(W_Now), TRAINING_PHI[i])), TRAINING_PHI[i])
         La_Delta_E_W = np.dot(La, W_Now)
@@ -222,7 +222,7 @@ def linearReggression( inputFilename):
         Erms_Test = GetErms(TEST_OUT, TestDataAct)
         L_Erms_Test.append(float(Erms_Test.split(',')[1]))
 
-    plt.plot(np.arange(500), L_Erms_Test)
+    plt.plot(np.arange(600), L_Erms_Test)
     plt.xlabel("Number of iterations")
     plt.ylabel("ERMS -Test")
     plt.show()
@@ -241,9 +241,9 @@ def linearReggression( inputFilename):
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def loss(h, y):
-    temp = (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
-    return temp
+def loss(hypo, target):
+    lossValue = (-target * np.log(hypo) - (1 - target) * np.log(1 - hypo)).mean()
+    return lossValue
 
 
 
@@ -280,46 +280,84 @@ def GenerateSubData(rawData, target, PercentDivision = 80):
 def logisticRegression(inputFilePath):
     normalData = pd.read_csv(inputFilePath)
     shuffleData = normalData.sample(frac=1)
+    # Need optimization for this code snipet
     shuffleData.to_csv('concat_shuffle', index=False)
     RawTarget = GetTargetVector('concat_shuffle')
     raw_data = pd.read_csv('concat_shuffle')
 
-    RawData = raw_data.iloc[:,3:21]
+    RawData = raw_data.iloc[:,2: len(raw_data.columns) -1 ]
     rawDataTraining,rawTargetTraining, rawDataTesting, rawTargetTesting = GenerateSubData(RawData, RawTarget)
 
     theta = np.zeros(len(RawData.iloc[0]))
     learning_rate = 0.01
-    erms_Training = []
-    erms_Testing = []
+    lossFunction = []
+
     for i in range(0,10000):
         z_train = np.dot(rawDataTraining, theta)
         h_train = sigmoid(z_train)
         temp =  h_train - rawTargetTraining
         gradient = np.dot(rawDataTraining.T , temp)/len(rawTargetTraining)
         theta = theta - learning_rate * gradient
-        erms_Training.append(loss(h_train,np.array(rawTargetTraining)))
+        lossFunction.append(loss(h_train,np.array(rawTargetTraining)))
     z_test = np.dot(rawDataTesting, theta)
     h_text = np.round(sigmoid(z_test))
     accuracy = (h_text == rawTargetTesting).mean()*100
 
 
-    print("E_rms Training    = " + str(np.around(min(erms_Training), 5)))
+
+
+    print("E_rms Training    = " + str(np.around(min(lossFunction), 5)))
     print("accuracy" + str(accuracy) + "%")
-    plt.plot(np.arange(10000), erms_Training)
+    plt.plot(np.arange(10000), lossFunction)
     plt.xlabel("Number of iterations")
-    plt.ylabel("ERMS -Test")
+    plt.ylabel("Loss Function")
     plt.show()
 
+def NeuralNetwork(inputFile, tag ):
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from keras.callbacks import EarlyStopping
 
 
+    inputDataDf = pd.read_csv(inputFile)
+    inputDataDf.sample(frac=1)
+    inputDataDf = inputDataDf.sample(frac=1)
+    dataDf = inputDataDf.iloc[:, 2: len(inputDataDf.columns) - 1]
+    targetdf = inputDataDf.iloc[:,-1]
+    model = Sequential()
+    model.add(Dense(150, input_dim= dataDf.shape[1] , activation = 'relu'))
+    model.add(Dense(250,  activation = 'relu'))
+    model.add( Dense(1))
+    model.compile(optimizer='adam',
+                  loss='mean_squared_error',
+                  metrics=['accuracy'])
+    earlystopping_cb = EarlyStopping(monitor='val_loss', verbose=1, patience=7, mode='min')
+    history = model.fit(dataDf
+                        , targetdf
+                        , validation_split=0.2
+                        , epochs=6000
+                        , callbacks=[ earlystopping_cb]
+                        )
+    df = pd.DataFrame(history.history)
+    df.plot(subplots=True, grid=True, figsize=(10, 15))
+    plt.savefig(tag + '.png')
 
 
-
+# lease uncomment the function you want to run
 
 if __name__ == '__main__':
-    #logisticRegression('concatenated_dataset.csv')
+
+
+    #NeuralNetwork('sub_HOF.csv', 'graph' )
+    #NeuralNetwork('concatenate_HOF.csv', 'graph_conc_hof' )
+    #NeuralNetwork('sub_GSC.csv', 'graph_sub_gsc' )
+    #NeuralNetwork('concatenate_GSC.csv', 'graph_conc_gsc' )
+    #logisticRegression('concatenate_HOF.csv')
+    logisticRegression('sub_HOF.csv')
+    #logisticRegression('concatenate_GSC.csv')
+    #logisticRegression('sub_GSC.csv')
     #linearReggression('concatenate_GSC.csv')
-    linearReggression('sub_GSC.csv')
+    #linearReggression('sub_GSC.csv')
     #linearReggression('concatenate_HOF.csv')
     #linearReggression('sub_HOF.csv')
 
